7fb8e6a7da8d5a4c3f287f54fe09d2055763d32a
ldoornenbal <31867982+ldoornenbal@users.noreply.github.com>
2018-04-05 15:42:37 +0200

    Let the motor go the same speed in the opposite direction

--- line-follower-v1.cpp
+++ line-follower-v1.cpp
@@ -48,7 +48,7 @@ int main(){
 			cout << "Right sees " << bwright.reflected << endl;
             if(bwright.reflected > 2100){
                 //set right motor speed to lower value
-                movement(30,-20);
+                movement(30,-30);
             }
             else{
                 //set right motor speed to normal value
@@ -62,7 +62,7 @@ int main(){
 			cout << "Left sees " << bwleft.reflected << endl;
             if(bwleft.reflected > 2100){
                 //set left motor speed to lower value
-                movement(-20,30);
+                movement(-30,30);
             }
             else{
                 //set left motor speed to normal value
